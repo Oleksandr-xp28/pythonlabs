c6a490be1a1a2d113c5ea1c99f4f7b424483e542
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-03-21 21:52:40 +0200

    Ex_3 task done

--- pythonlabs.py
+++ pythonlabs.py
@@ -1,1 +1,10 @@
+if __name__ == "__main__":
+    try:
+        first_num = input("Enter the first digit: ")
+        second_num = input("Enter the second digit: ")
+        number = int(first_num + second_num)
+        print(f'{number}')
+    except Exception as ex:
+        print(f'log: {ex}')
+
 
